1a4465771e30edf4a5c17ed89e7b7896dff3b98b
Reginaldo Junior <76regi@gmail.com>
2016-04-09 16:08:35 -0300

    ok

--- api/vendor/autoload.php
+++ api/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd41e500974ffd4468bbbbdbb4c9704ce::getLoader();
+return ComposerAutoloaderInitabb8786647ff20b1ce43fcd43abded0e::getLoader();
